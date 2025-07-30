f0fcdbf8c2f5c905ffa9fa9e176121d5ca706ded
marcusaferreira <marcus_aferreira@hotmail.com>
2025-07-30 11:05:24 -0300

    feat: Adding java project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import static java.lang.System.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            out.println("i = " + i);
+        }
+    }
+}
